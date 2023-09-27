25ccb8ea0ae07a64c848a796aeb1895aa2e0ca6e
James <james_teo@tech.gov.sg>
2023-09-27 16:18:35 +0800

    day 3 update

--- day1/workshop1/task2/steps.sh
+++ day1/workshop1/task2/steps.sh
@@ -3,7 +3,7 @@ docker volume create my-volume
 # Database
 docker run -d --network my-bridge --name mydb  --mount type=volume,src=my-volume,dst=/var/lib/mysql ghcr.io/jamestth/northwind-db:v1
 # MyApp
-docker run -d --network my-bridge --name myapp -p 8080:3000 -e DB_HOST=northwind -e DB_PASSWORD=changeit -e DB_HOST=mydb  stackupiss/northwind-app:v1 
+docker run -d --network my-bridge --name myapp -p 8080:3000 -e DB_USER=northwind -e DB_PASSWORD=changeit -e DB_HOST=mydb  stackupiss/northwind-app:v1 
 
 
 # pinging localhost hits this however, but i think the tutorial is more about the networking and mounting of volume
